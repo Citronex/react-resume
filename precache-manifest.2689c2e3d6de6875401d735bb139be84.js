bb304e1da75b0f1a0e30ea4af2f1f6a386766ef5
Francisco Mejias <fxmejias@gmail.com>
2019-03-18 18:41:21 -0400

    Updates

---
+++ precache-manifest.2689c2e3d6de6875401d735bb139be84.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1012a40903ca5de0bcf",
+    "url": "/react-resume/static/css/main.07318036.chunk.css"
+  },
+  {
+    "revision": "f1012a40903ca5de0bcf",
+    "url": "/react-resume/static/js/main.5a343dc3.chunk.js"
+  },
+  {
+    "revision": "9ebccc51354bc6ce9a39",
+    "url": "/react-resume/static/js/runtime~main.bbd03709.js"
+  },
+  {
+    "revision": "1e79f011ff9c15bf1d8b",
+    "url": "/react-resume/static/js/2.b5df8322.chunk.js"
+  },
+  {
+    "revision": "3e6a81778e6080575e8cc0ba8c29e2ef",
+    "url": "/react-resume/index.html"
+  }
+];
